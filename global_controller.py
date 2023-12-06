3944d66e5aaa2df6c2877a2517d61b6f6bc14e5e
Yuqi <leozhangyuqi@gmail.com>
2023-12-06 13:02:51 -0500

    finished

--- global_controller.py
+++ global_controller.py
@@ -2,6 +2,7 @@ import time
 import threading
 import requests
 import logging
+from datetime import datetime
 
 # APIs
 get_nodes_api = "localhost:5001/nodes"
@@ -19,6 +20,20 @@ worker_nodes = [
     "k8s-worker1",
     "k8s-worker2",
 ]  # list of the two workers, in the order of jobs assignemnt priority, e.g., job will be assigned to master node, if unable, to the worker1, then worker2
+node_job_api = {
+    "k8s-master": "localhost:5002/job",
+    "k8s-worker1": "localhost:5002/job",
+    "k8s-worker2": "localhost:5002/job",
+}
+cpu_bar = 0.8
+number_cpu_data_used = (
+    6  # use the previous X number of cpu to see if we need to scale up
+)
+node_start_delay = (
+    30  # no scaling down decision in X seconds after a scaling up decision
+)
+job_assign_time = 15  # every X seconds, schedule a job
+job_file_name = "job_list.txt"
 
 # global variables
 # CPU_usage = {
@@ -28,6 +43,22 @@ worker_nodes = [
 # }
 cluster_cpu = []  # recent cluster CPU usage
 started_nodes = [master_node]  # the nodes that have been started by the controller.
+last_started_time = datetime.now()
+job_list = []
+
+
+def read_file_to_list(file_path):
+    """read a file, return a list of strings(each line)"""
+    try:
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+        # Strip newline characters from each line
+        lines = [line.strip() for line in lines]
+        return lines, None
+    except FileNotFoundError:
+        return [], "The file was not found."
+    except Exception as e:
+        return [], e
 
 
 def get_nodes():
@@ -59,15 +90,189 @@ def get_cpu():
         return None, e
 
 
+def delete_node(node_name):
+    try:
+        payload = {"node": node_name}
+        response = requests.post(delete_node_api, json=payload)
+        if response.status_code == 200:
+            res = response.json()
+            return res["success"], res["msg"]
+        else:
+            return False, f"Error: {response.status_code}"
+    except Exception as e:
+        return False, e
+
+
+def start_new_node(node_name):
+    try:
+        payload = {"node": node_name}
+        response = requests.post(start_node_api, json=payload)
+        if response.status_code == 200:
+            res = response.json()
+            return res["success"], res["msg"]
+        else:
+            return False, f"Error: {response.status_code}"
+    except Exception as e:
+        return False, e
+
+
+def remove_worker(node_name):
+    global worker_nodes, started_nodes
+    index = 0
+    for node in worker_nodes:
+        if node == node_name:
+            break
+        index += 1
+    worker_nodes.pop(index)
+
+    index = 0
+    for node in started_nodes:
+        if node == node_name:
+            break
+        index += 1
+    started_nodes.pop(index)
+
+
 def sample_cpu():
     """sample the cluster CPU"""
-    global sample_time
+    global sample_time, started_nodes
     while True:
-        nodes, err = get_nodes()
+        running_nodes, err = get_nodes()
+        logging.debug(f"running nodes: {running_nodes}")
         if err != None:
             logging.critical(f"error getting nodes, msg: {err}")
-        cpu, err = get_cpu()
+            time.sleep(sample_time)
+            continue
+        nodes_cpu, err = get_cpu()
+        logging.debug(f"nodes_cpu: {nodes_cpu}")
         if err != None:
             logging.critical(f"error getting nodes cpu, msg: {err}")
+            time.sleep(sample_time)
+            continue
+        total_cpu = 0
+        num = 0
+        for node in started_nodes:
+            if node not in running_nodes:
+                logging.error(f"node started but not currently running: {node}")
+                logging.info(
+                    f"removing node {node} from worker nodes because it stops accidentally"
+                )
+                remove_worker(node)
 
-        time.sleep(sample_cpu)
+            if node not in nodes_cpu:
+                logging.error(f"can't get node CPU, node: {node}")
+            total_cpu += nodes_cpu[node] / 100
+            num += 1
+        if num != 0:
+            cur_cluster_cpu = total_cpu / num
+            cluster_cpu.append(cur_cluster_cpu)
+            logging.info(f"current cluster cpu: {cur_cluster_cpu}")
+        time.sleep(sample_time)
+
+
+def controller():
+    """make scaling up of scaling down decision"""
+    global started_nodes, cluster_cpu, number_cpu_data_used, cpu_bar
+    while True:
+        # scaling up decision
+        # compute cluster cpu
+        ave_cluster_cpu = None
+        if len(cluster_cpu) < number_cpu_data_used:
+            logging.info("not enough CPU data, skip scaling up")
+        else:
+            cpu_data = cluster_cpu[-number_cpu_data_used:]
+            ave_cluster_cpu = sum(cpu_data) / number_cpu_data_used
+        if ave_cluster_cpu > cpu_bar:
+            # scale up
+            if len(started_nodes) == len(worker_nodes) + 1:
+                logging.info("all nodes started, won't scale up")
+            logging.info(
+                f"current cluster average {ave_cluster_cpu}, greater than {cpu_bar}, scaling up"
+            )
+            new_node = worker_nodes[
+                len(started_nodes) - 1
+            ]  # master node is always started
+            started_nodes(new_node)
+            last_started_time = datetime.now()
+        else:
+            logging.info(
+                f"current cluster average {ave_cluster_cpu}, less than {cpu_bar}, not scaling up"
+            )
+        # scaling down decision
+        if (
+            datetime.now() - last_started_time
+        ).total_seconds() > node_start_delay and started_nodes > 1:
+            # check last node jobs, if it's zero, delete it
+            target_node = started_nodes.pop()
+            ok, e = delete_node(target_node)
+            if not ok:
+                logging.error(f"error when deleting node {target_node}, error: {e}")
+        time.sleep(loop_sleep_time)
+
+
+def assign_job(job, node_name):
+    """try to assign a job to a node
+
+    Args:
+        job (_type_): _description_
+        node (_type_): _description_
+    """
+    try:
+        payload = {"node": node_name, "job": job}
+        response = requests.post(node_job_api[node_name], json=payload)
+        if response.status_code == 200:
+            res = response.json()
+            return res["success"], res["msg"]
+        else:
+            return False, f"Error: {response.status_code}"
+    except Exception as e:
+        return False, e
+
+
+def job_scheduling():
+    while True:
+        job = job_list[0]
+        job_assigned = False
+        for node in started_nodes:
+            ok, err = assign_job(job, node)
+            if not ok:
+                logging.info(f"can't assign job {job} to node {node}, because {err}")
+            else:
+                logging.info(f"assgiend job {job} to node {node}")
+                job_assigned = True
+                break
+        if not job_assigned:
+            logging.info(
+                f"job {job} can't be assigned, will try to assign the job in the next iteration"
+            )
+        else:
+            job_list = job_list[1:]
+        time.sleep(job_assign_time)
+
+
+if __name__ == "__main__":
+    # read job list
+    job_list, error = read_file_to_list(job_file_name)
+    logging.info(f"getting job list from {job_file_name}")
+    if error != None:
+        logging.critical(f"error getting the job list: {error}")
+        logging.critical("shutting down")
+        exit(0)
+
+    # start CPU sampling
+    logging.info("start sampling")
+    sample_cpu_thread = threading.Thread(target=sample_cpu)
+    sample_cpu_thread.daemon = True
+    sample_cpu_thread.start()
+
+    # start controller
+    logging.info("start controller")
+    controller_thread = threading.Thread(target=controller)
+    controller_thread.daemon = True
+    controller_thread.start()
+
+    # start job rendering
+    logging.info("start controller")
+    job_thread = threading.Thread(target=job_scheduling)
+    job_thread.daemon = True
+    job_thread.start()
